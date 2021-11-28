375383f2d2472923bf8caae483b96e618f24bbb9
chillenb <chillenbrand15@gmail.com>
2021-11-28 16:10:52 -0500

    Added a tsp_batch.py script to run batches for benchmarking

--- tsp/tsp_main.py
+++ tsp/tsp_main.py
@@ -12,35 +12,37 @@ def main():
     parser.add_argument('-time', type=int, dest='maxtime', default=10, help='The cutoff time of the algorithm')
     parser.add_argument('-odir', type=str, dest='odir', default=".", help='Where to store output files')
     args = parser.parse_args()
+    run_tsp_main(args.inst, args.algorithm, args.seed, args.maxtime, args.odir)
 
+def run_tsp_main(inst, algorithm, seed, maxtime, odir):
     #Get the predefined class of TSP data using given data_path
-    data = parse(args.inst)
+    data = parse(inst)
     dist_matrix = data.to_adjacency_mat()
-    if args.algorithm == "LS1" or args.algorithm == "LS2":
-        sol_path = args.odir + "/{}_{}_{}_{}.sol".format(Path(args.inst).stem, args.algorithm, args.maxtime, args.seed)
-        trace_path = args.odir + "/{}_{}_{}_{}.trace".format(Path(args.inst).stem, args.algorithm, args.maxtime, args.seed)
+    if algorithm == "LS1" or algorithm == "LS2":
+        sol_path = odir + "/{}_{}_{}_{}.sol".format(Path(inst).stem, algorithm, maxtime, seed)
+        trace_path = odir + "/{}_{}_{}_{}.trace".format(Path(inst).stem, algorithm, maxtime, seed)
     else:
-        sol_path = args.odir + "/{}_{}_{}.sol".format(Path(args.inst).stem, args.algorithm, args.maxtime)
-        trace_path = args.odir + "/{}_{}_{}_{}.trace".format(Path(args.inst).stem, args.algorithm, args.maxtime, args.seed)
+        sol_path = odir + "/{}_{}_{}.sol".format(Path(inst).stem, algorithm, maxtime)
+        trace_path = odir + "/{}_{}_{}_{}.trace".format(Path(inst).stem, algorithm, maxtime, seed)
 
-    if args.algorithm == 'BnB':
+    if algorithm == 'BnB':
         #TODO: Run Branch and Bound 
         print("Use the BnB algorithm")
-    elif args.algorithm == 'Approx':
+    elif algorithm == 'Approx':
         #TODO: Run Approximate Solution
         print("Use the Approx algorithm")
-    elif args.algorithm == 'LS1':
+    elif algorithm == 'LS1':
         #TODO: Run the LS1 (Simulated Annealing)Solution
-        sol_instance = Local_Search_SA.LS1_SA(dist_matrix, 0.00001, args.seed, args.maxtime)
+        sol_instance = Local_Search_SA.LS1_SA(dist_matrix, 0.00001, seed, maxtime)
         solution = sol_instance.Simulated_Annealing()
-        print("The min distance of {} is {}".format(args.algorithm, solution[0]))
+        print("The min distance of {} is {}".format(algorithm, solution[0]))
         utils.gen_solution_file(solution[0], solution[1], sol_path)
         utils.gen_trace_file(solution[2], trace_path)
-    elif args.algorithm == 'LS2':
+    elif algorithm == 'LS2':
         #TODO: Run the LS2 Solution
         print("Use the LS2 algorithm")
     else:
-        print("The {} algorithm is not supported, please try another".format(args.algorithm))
+        print("The {} algorithm is not supported, please try another".format(algorithm))
     
 if __name__ == '__main__':
     main()
