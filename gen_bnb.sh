733e327da41c4b4e764e01575d812d457244a0c5
chillenb <chillenbrand15@gmail.com>
2021-12-02 23:35:22 +0000

    appendtottime is default in gen_bnb.sh

--- gen_bnb.sh
+++ gen_bnb.sh
@@ -2,11 +2,11 @@
 
 for instance in DATA/*.tsp
 do
-    for alg in Approx BnB
+    for alg in BnB
     do
         city="$(basename -- $instance .tsp)"
         mkdir -p "batchout/$city$alg"
         mydir="batchout/$city$alg"
-        python3 -m tsp.tsp_batch -batch-size 1 -alg $alg -inst $instance -odir $mydir -time 600
+        python3 -m tsp.tsp_batch -batch-size 1 -alg $alg -inst $instance -odir $mydir -time 600 -appendtottime
     done
 done
